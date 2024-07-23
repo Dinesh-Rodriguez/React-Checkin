49793e85d03f4b6640c2ad868cae7b4701adbedd
Dinesh-Rodriguez <antondinesh2021@gmail.com>
2024-07-23 23:35:52 +0530

    Destructuring in parametar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Greet name="Diana" heroName="Wonder Woman"/>
+      {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman" >
         <p> This is children props </p>
